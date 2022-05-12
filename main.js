435f5ca183c19b687ee853754900a49e12d42bb3
eric2408 <105270650+eric2408@users.noreply.github.com>
2022-05-12 14:57:54 -0700

    js: nav button

--- main.js
+++ main.js
@@ -9,4 +9,18 @@ document.addEventListener('scroll', () => {
     } else {
         navbar.classList.remove('navbar-dark');
     }
-})
+});
+
+
+//handle scrolling when tapping on navbar menu//
+const navbarMenu = document.querySelector('.navbar_menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+});

19d9b49caabd65fb8845ee02bdf09f2cae6a1647
eric2408 <105270650+eric2408@users.noreply.github.com>
2022-05-12 17:48:55 -0700

    js: project filtering

--- main.js
+++ main.js
@@ -53,7 +53,31 @@ arrowUp.addEventListener('click', ()=>{
     scrollIntoView('#home');
 });
 
+//projects
 
+const workBtnContainer = document.querySelector('.work_categories');
+const projectContainer = document.querySelector('.work_projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (event)=>{
+    const filter = event.target.dataset.filter || event.target.parentNode.dataset.filter;
+    if(filter== null){
+        return;
+    }
+    projectContainer.classList.add('anim-out');
+    
+
+    setTimeout(() => {
+        projects.forEach((project) => {
+            if(filter === '*' || filter === project.dataset.type){
+                project.classList.remove('invisible');
+            } else {
+                project.classList.add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anim-out');
+    }, 300);
+    
+});
 
 
 function scrollIntoView(selector){
